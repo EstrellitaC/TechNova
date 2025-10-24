bf3ceaba948de6202a70d26d72f15d0aecb43976
EstrellitaC <127231304+EstrellitaC@users.noreply.github.com>
2025-10-24 12:13:59 -0500

    Delete technova_php_codebase directory

--- technova_php_codebase/lib/auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-// lib/auth.php
-require_once __DIR__ . '/../config/config.php';
-if(!is_logged()){ redirect('login.php'); }
-?>
--- technova_php_codebase/public/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/../config/config.php';
-session_destroy();
-redirect('index.php');
-?>
